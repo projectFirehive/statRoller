b97e9b0a5dd009b5ab37308f20319de0e24f3d54
projectFirehive <projectFirehive@gmail.com>
2022-06-15 23:23:20 +0100

    Opened repo and uploaded initial project files

---
+++ statRoller2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time())
+    return 0;
+}
+int statGen()
+{
+    int die1 = 1+ (rand()%6);
+    int die2 = 1+ (rand()%6);
+    int die3 = 1+ (rand()%6);
+    int die4 = 1+ (rand()%6);
+}
